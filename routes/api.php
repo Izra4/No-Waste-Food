24393812dbda2e24f0fc03f8a6e31886008d05c0
izra4 <aryaizra2@gmail.com>
2024-05-25 17:24:14 +0700

    chore: edit csrf verify setting

--- routes/api.php
+++ routes/api.php
@@ -2,5 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+
+Route::get("/product/{id}",[ProductController::class, 'findByID']);
+Route::post("/product/create",[ProductController::class, 'store']);
 
 
+Route::get("/test", [ProductController::class, 'test']);
