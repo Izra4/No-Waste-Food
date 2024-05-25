55ee1670c2d07945fcee490e5663ab5c4b87e5d5
izra4 <aryaizra2@gmail.com>
2024-05-25 17:25:04 +0700

    refactor: fix product controller and model

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,57 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ProductCreateRequest;
+use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class ProductController extends Controller
 {
-    //
+    public function create()
+    {
+        log::info("Masuk ke form");
+        $categories = Category::all();
+        return view('products.create', compact('categories'));
+    }
+    public function store(ProductCreateRequest $request)
+    {
+        log::info("Masuk ke store");
+
+        $validated = $request->validated();
+
+        $product = Product::create([
+            'name' => $validated['name'],
+            'description' => $validated['description'],
+            'image' => $validated['image'],
+            'price' => $validated['price'],
+            'stock' => $validated['stock'],
+        ]);
+
+        $product->categories()->attach($validated['categories']);
+
+        return response()->json([
+            "message" => "Product created successfully",
+        ]);
+    }
+
+    public function findByID($id)
+    {
+        $product = Product::with('categories')->findOrFail($id);
+        $arr = $product->toArray();
+        $arr['categories'] = $product->categories->pluck('name');
+
+        return response()->json($arr);
+    }
+
+    //want to make api for update product by id
+
+    public function test()
+    {
+        log::info("Hello World");
+        return response()->json([
+            'message' => 'Hello World',
+        ]);
+    }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
@@ -9,11 +10,21 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 class Product extends Model
 {
     use HasFactory;
+    use HasUuids;
 
-    protected $table = 'product';
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $fillable = [
+        'name',
+        'description',
+        'image',
+        'price',
+        'stock',
+    ];
 
     public function categories(): BelongsToMany
     {
-        return $this->belongsToMany(Category::class);
+        return $this->belongsToMany(Category::class, 'category_product', 'product_id', 'category_id');
     }
+
 }
