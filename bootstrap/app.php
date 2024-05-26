a15a6d02b5cc20bc7fa50d9d497c5df8ada713dc
izra4 <aryaizra2@gmail.com>
2024-05-26 21:55:05 +0700

    refactor: fix csrf verify setting

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->validateCsrfTokens(except: [
-            '/product/create'
+            'http://localhost:8000/api/product/*',
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
